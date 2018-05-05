e2d9b44baa68987c54660a1d51cef5323da386f2
omyrobin <omy_robin@126.com>
2018-05-05 11:29:07 +0800

    合并

--- app/src/main/java/com/administration/bureau/constant/Url.java
+++ app/src/main/java/com/administration/bureau/constant/Url.java
@@ -13,21 +13,4 @@ public class Url {
 
     public static final String GENERATE = "http://106.39.33.2:7070/generate/notice.jpg";
 
-    public void create(){
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                //创建Looper对象，并保存到ThreadLocal中
-                Looper.prepare();
-                //从ThreadLocal中取出当前线程的Looper对象
-                Looper looper = Looper.myLooper();
-                //使用Looper对象创建Handler，使Hander关联当前线程的Looper
-                Handler handler = new Handler(looper);
-                //开启Looper循环
-                Looper.loop();
-            }
-        }).start();
-    }
-
-
 }
